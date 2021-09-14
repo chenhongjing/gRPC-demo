645542c993a76a3dae40f4ab1d3777c5296864d3
chenhongjing <60866283+chenhongjing@users.noreply.github.com>
2021-09-14 16:31:56 +0800

    trial

--- client.py
+++ client.py
@@ -10,21 +10,31 @@ queue = Queue()
 
 
 def main():
+    count = 0
     with grpc.insecure_channel("localhost:50051") as channel:
         stub = schema_pb2_grpc.GatewayStub(channel)
         queue.put(1)
-        resp = stub.Call(generate_message())
-        for r in resp:
+        # resp = stub.Call(generate_message())
+        # for r in resp:
+        #     num = r.num
+        #     queue.put(num)
+
+        # modified
+        for r in stub.Call(generate_message()):
             num = r.num
-            queue.put(num)
+            count += 1
+            if count <= 5:
+                queue.put(num)
 
 
 def generate_message():
     while True:
-        num = queue.get()
-        print(num)
-        yield schema_pb2.Request(num=num)
-        # yield schema_pb2.Request()
+        try:
+            num = queue.get(timeout=5)
+            print(num)
+            yield schema_pb2.Request(num=num)
+        except Exception:
+            print("no")
 
 
 if __name__ == "__main__":
